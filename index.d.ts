3e03041fb4b773b54ec1056e0f9fb66f61106408
Daniel K <FredyC@users.noreply.github.com>
2019-02-25 02:23:59 +0100

    fix(ts): Update typings to allow additional DocumentNode (#78)

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 import { DocumentNode } from 'graphql';
 
 export function loader(path: string): DocumentNode;
-export function gql(taggedTemplateLiteral: TemplateStringsArray): DocumentNode;
+export function gql(taggedTemplateLiteral: TemplateStringsArray, ...nodes: DocumentNode[]): DocumentNode;
